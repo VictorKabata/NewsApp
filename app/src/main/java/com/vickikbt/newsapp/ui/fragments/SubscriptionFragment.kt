05c7761b131f64bfc76cf0fc4ecebc1fe46a66ba
Job Getabu <getabujob@gmail.com>
2021-02-10 00:32:43 +0300

    add v2 lib

--- app/src/main/java/com/vickikbt/newsapp/ui/fragments/SubscriptionFragment.kt
+++ app/src/main/java/com/vickikbt/newsapp/ui/fragments/SubscriptionFragment.kt
@@ -1,5 +1,6 @@
 package com.vickikbt.newsapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +11,22 @@ import androidx.navigation.fragment.findNavController
 import com.vickikbt.newsapp.R
 import com.vickikbt.newsapp.databinding.FragmentSubscriptionBinding
 import com.vickikbt.newsapp.ui.adapters.BillingBottomFragment
+import com.vickikbt.newsapp.util.Constants
+import com.vickikbt.newsapp.util.toast
+import net.kosev.billing.Billing
+import net.kosev.billing.Billing.*
+import net.kosev.billing.Inventory
+import net.kosev.billing.Purchase
+
+
+
 
 
 class SubscriptionFragment : Fragment() {
 
+    private val REQUEST_PURCHASE = 1001
     private lateinit var binding: FragmentSubscriptionBinding
+    private var mBilling: Billing? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,12 +48,77 @@ class SubscriptionFragment : Fragment() {
     private fun initUI() {
         val billingBottomFragment = BillingBottomFragment()
 
+
+        mBilling = Billing(requireContext(), Constants.LICENSE_KEY)
+        mBilling!!.create(object : CreateListener {
+            override fun onSuccess() {
+                val skus: ArrayList<String> = ArrayList()
+                skus.add("intermediate")
+                skus.add("expert")
+                mBilling!!.loadInventory(skus, object : InventoryListener {
+                    override fun onSuccess(inventory: Inventory) {
+                        // inventory.hasPurchase("Your SKU");
+                    }
+
+                    override fun onError(response: Int, e: Exception) {
+                        // show error
+                        requireActivity().toast(e.localizedMessage)
+                    }
+                })
+            }
+
+            override fun onError(response: Int, e: Exception) {
+                // billing is not supported
+            }
+        })
+
+
         binding.buttonSubscribeRate1.setOnClickListener {
-            billingBottomFragment.show(childFragmentManager, "intermediate")
+            //billingBottomFragment.show(childFragmentManager, "intermediate")
+
+            mBilling!!.launchPurchaseFlow(requireActivity(),
+                "intermediate",
+                TYPE_SUBS,
+                REQUEST_PURCHASE,
+                object : PurchaseListener {
+                    override fun onSuccess(purchase: Purchase) {
+                        if ("intermediate" == purchase.sku) {
+                            // complete order
+                        }
+                    }
+
+                    override fun onError(response: Int, e: Exception) {
+                        // show error
+                        requireActivity().toast(e.localizedMessage)
+                    }
+                })
         }
 
         binding.buttonSubscribeRate2.setOnClickListener {
-            billingBottomFragment.show(childFragmentManager, "expert")
+            //billingBottomFragment.show(childFragmentManager, "expert")
+            mBilling!!.launchPurchaseFlow(requireActivity(),
+                "expert",
+                TYPE_SUBS,
+                REQUEST_PURCHASE,
+                object : PurchaseListener {
+                    override fun onSuccess(purchase: Purchase) {
+                        if ("expert" == purchase.sku) {
+                            // complete order
+                        }
+                    }
+
+                    override fun onError(response: Int, e: Exception) {
+                        // show error
+                        requireActivity().toast(e.localizedMessage)
+                    }
+                })
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (mBilling != null) {
+            mBilling!!.onActivityResult(requestCode, resultCode, data);
         }
     }
 
