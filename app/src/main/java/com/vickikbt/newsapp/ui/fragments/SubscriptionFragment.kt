ab24a95f6284fc80513b7b8ee92740d73ba2a00a
Job Getabu <getabujob@gmail.com>
2021-02-13 20:05:32 +0300

    added billing stuff

--- app/src/main/java/com/vickikbt/newsapp/ui/fragments/SubscriptionFragment.kt
+++ app/src/main/java/com/vickikbt/newsapp/ui/fragments/SubscriptionFragment.kt
@@ -1,5 +1,6 @@
 package com.vickikbt.newsapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +8,19 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.anjlab.android.iab.v3.BillingProcessor
+import com.anjlab.android.iab.v3.TransactionDetails
 import com.vickikbt.newsapp.R
 import com.vickikbt.newsapp.databinding.FragmentSubscriptionBinding
 import com.vickikbt.newsapp.ui.adapters.BillingBottomFragment
+import com.vickikbt.newsapp.util.toast
 
 
-class SubscriptionFragment : Fragment() {
+class SubscriptionFragment : Fragment(), BillingProcessor.IBillingHandler {
 
     private lateinit var binding: FragmentSubscriptionBinding
+
+    var bp: BillingProcessor? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +34,9 @@ class SubscriptionFragment : Fragment() {
             findNavController().navigateUp()
         }
 
+        bp = BillingProcessor(requireContext(), "YOUR LICENSE KEY FROM GOOGLE PLAY CONSOLE HERE", this)
+        bp?.initialize()
+
         initUI()
 
         return binding.root
@@ -37,12 +46,39 @@ class SubscriptionFragment : Fragment() {
         val billingBottomFragment = BillingBottomFragment()
 
         binding.buttonSubscribeRate1.setOnClickListener {
-            billingBottomFragment.show(childFragmentManager, "intermediate")
+            //billingBottomFragment.show(childFragmentManager, "intermediate")
+            bp?.subscribe(requireActivity(), "intermediate")
         }
 
         binding.buttonSubscribeRate2.setOnClickListener {
-            billingBottomFragment.show(childFragmentManager, "expert")
+            //billingBottomFragment.show(childFragmentManager, "expert")
+            bp?.subscribe(requireActivity(), "expert")
         }
     }
 
+    override fun onBillingInitialized() {
+
+    }
+
+    override fun onProductPurchased(productId: String, details: TransactionDetails?) {
+
+    }
+
+    override fun onPurchaseHistoryRestored() {
+
+    }
+
+    override fun onBillingError(errorCode: Int, error: Throwable?) {
+        requireActivity().toast("${error?.localizedMessage}")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
+
+    override fun onDestroy() {
+        bp?.release()
+        super.onDestroy()
+    }
 }
